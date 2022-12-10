561523ccbebf8c8d05ef299f5e0b72662fe579ff
Chandan <cchandel@uwo.ca>
2022-12-10 01:06:56 -0500

    final version

---
+++ AddressBook/addressbook.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QtWidgets/QWidget>
+#include "ui_addressbook.h"
+
+class AddressBook : public QWidget
+{
+    Q_OBJECT
+
+public:
+    AddressBook(QWidget *parent = nullptr);
+    ~AddressBook();
+
+private:
+    Ui::AddressBookClass ui;
+
+private slots:
+    void on_addButton_clicked();
+    void on_addressList_currentItemChanged();
+    void on_deleteButton_clicked();
+};
